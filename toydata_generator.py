f96767a2a74c8e6d1ed9d8b2ee1155fa6484d53f
Temigo <ldomine@stanford.edu>
2018-02-15 19:57:38 -0500

    Update toy generator

--- toydata_generator.py
+++ toydata_generator.py
@@ -8,6 +8,8 @@ from track_generator import generate_toy_tracks
 # from shower_generator import *
 
 class ToydataGenerator(object):
+    CLASSES = ('__background__', 'track_start', 'track_end', 'shower_start')
+
     def __init__(self, N, max_tracks, max_kinks):
         self.N = N
         self.max_tracks = max_tracks
@@ -17,8 +19,25 @@ class ToydataGenerator(object):
         output = np.zeros(shape=(self.N, self.N), dtype=int)
         output_tracks, track_start_points, track_end_points = generate_toy_tracks(self.N, self.max_tracks, max_kinks=self.max_kinks)
         # output_shower =
+        track_labels = []
+        for i in range(track_start_points):
+            track_labels.append([
+                track_start_points[i][0]-2,
+                track_start_points[i][1]-2,
+                track_start_points[i][0]+2,
+                track_start_points[i][1]+2,
+                1 # track_start
+            ])
+            track_labels.append([
+                track_end_points[i][0]-2,
+                track_end_points[i][1]-2,
+                track_end_points[i][0]+2,
+                track_end_points[i][1]+2,
+                2 # track end
+            ])
+
         blob['image'] = output
-        # blob['gt'] =
+        blob['gt_boxes'] = np.array(track_labels)
         return blob
 
 if __name__ == '__main__':
