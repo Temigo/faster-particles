d83f00c1df51a32d7543ebf9bd9e704dbb49f9cf
Temigo <temigo@gmx.com>
2018-03-01 14:16:41 -0500

    Add test_predicted_pixels2.

--- test_ppn.py
+++ test_ppn.py
@@ -83,11 +83,13 @@ class Test(unittest.TestCase):
         R = 20
         width, height = 2, 2
         repeat = 1
-        anchors_np = generate_anchors_np(width=width, height=height, repeat=repeat)
-        # Shape [None, N, N, n] where n = 2 (background/signal) or num_classes
+
+        # Shape [None, N, N, n] where n = 2 (background/signal)
         rpn_cls_prob_np = np.array([[[[0.1, 0.9], [0.3, 0.7]], [[0.5, 0.5], [0.8, 0.2]]]])
         # Shape [None, N, N, 2]
         rpn_bbox_pred_np = np.array([[[[0.1, 0.1], [0.5, 0.2]], [[0.9, -0.5], [0.1, -0.4]]]])
+
+        anchors_np = generate_anchors_np(width=width, height=height, repeat=repeat)
         scores = rpn_cls_prob_np[:, :, :, 1:]
         roi_scores_np = np.reshape(scores, (-1, 1))
         anchors_np = np.reshape(anchors_np, (-1, rpn_cls_prob_np.shape[1], rpn_cls_prob_np.shape[1], 2))
@@ -96,6 +98,7 @@ class Test(unittest.TestCase):
         # clip predicted pixels to the image
         proposals = clip_pixels(proposals) # FIXME np function
         rois_np = proposals.astype(float)
+
         with tf.Session() as sess:
             anchors_tf = generate_anchors(width=width, height=height, repeat=repeat)
             rpn_cls_prob_tf = tf.constant(rpn_cls_prob_np, dtype=tf.float32)
@@ -105,7 +108,32 @@ class Test(unittest.TestCase):
             return np.allclose(rois_tf, rois_np) and np.allclose(roi_scores_tf, roi_scores_np)
 
     def test_predicted_pixels2(self): # with classes=True ~ for PPN2
-        pass
+        R = 20
+        width, height = 2, 2
+        repeat = 1
+
+        # Shape [None, N, N, n] where n = num_classes
+        rpn_cls_prob_np = np.array([[[[0.1, 0.8, 0.1], [0.3, 0.65, 0.05]], [[0.5, 0.02, 0.48], [0.8, 0.18, 0.02]]]])
+        # Shape [None, N, N, 2]
+        rpn_bbox_pred_np = np.array([[[[0.1, 0.1], [0.5, 0.2]], [[0.9, -0.5], [0.1, -0.4]]]])
+
+        anchors_np = generate_anchors_np(width=width, height=height, repeat=repeat)
+        roi_scores_np = scores = np.reshape(rpn_cls_prob_np, (-1, rpn_cls_prob_np.shape[-1]))
+        anchors_np = np.reshape(anchors_np, (-1, rpn_cls_prob_np.shape[1], rpn_cls_prob_np.shape[1], 2))
+        proposals =  anchors_np + rpn_bbox_pred_np
+        proposals = np.reshape(proposals, (-1, 2))
+        # clip predicted pixels to the image
+        proposals = clip_pixels(proposals) # FIXME np function
+        rois_np = proposals.astype(float)
+
+        with tf.Session() as sess:
+            anchors_tf = generate_anchors(width=width, height=height, repeat=repeat)
+            rpn_cls_prob_tf = tf.constant(rpn_cls_prob_np, dtype=tf.float32)
+            rpn_bbox_pred_tf = tf.constant(rpn_bbox_pred_np, dtype=tf.float32)
+            rois, roi_scores = predicted_pixels(rpn_cls_prob_tf, rpn_bbox_pred_tf, anchors_tf, R=R, classes=True)
+            rois_tf, roi_scores_tf = sess.run([rois, roi_scores])
+            return np.allclose(rois_tf, rois_np) and np.allclose(roi_scores_tf, roi_scores_np)
+
 
     def test_include_gt_pixels(self):
         pass
@@ -134,7 +162,7 @@ class Test(unittest.TestCase):
         closest_gt_distance_test = np.arange(nb_rois*n*n).reshape(nb_rois*n*n, 1)
         true_labels_test = np.ones((nb_rois*n*n, 1))
         thres_test = 20
-        
+
         common_shape_np = np.array([nb_rois*n*n, 1])
         predicted_labels_np = np.argmax(scores_test, axis=1).reshape(common_shape_np)
         #print(predicted_labels_np.shape)
@@ -145,10 +173,10 @@ class Test(unittest.TestCase):
             mask_tf = compute_positives_ppn2(scores_test, closest_gt_distance_test, true_labels_test, threshold=thres_test)
             mask_tf = sess.run([mask_tf])
         return np.allclose(mask_np, mask_tf)
-        
+
     def test_assign_gt_pixels(self):
         # Dummy input for testing
-        N = 2 
+        N = 2
         gt_pixels_placeholder_test = np.empty((N*N, 2))
         pass
 
