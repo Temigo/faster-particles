Fix bug in loss_ppn2_class computation (true labels were for closest gt pixel of same class only)

ppn.py
@@ -59,7 +59,7 @@ class PPN(object):
         feed_dict = { self.image_placeholder: blobs['data'], self.gt_pixels_placeholder: blobs['gt_pixels'] }
         _, ppn1_pixel_pred, ppn1_cls_prob, ppn1_anchors, ppn1_proposals, \
         ppn1_scores, labels_ppn1, rois, ppn2_anchors, ppn2_proposals, ppn2_positives, \
-        ppn2_scores, ppn2_closest_gt_distance, ppn2_true_labels, ppn2_cls_score, \
+        ppn2_scores, ppn2_closest_gt_distance, ppn2_true_labels, ppn2_true_labels_both, ppn2_cls_score, \
         loss_ppn2_point, summary = sess.run([
                             self.train_op,
                             self._predictions['ppn1_pixel_pred'],
@@ -75,6 +75,7 @@ class PPN(object):
                             self._predictions['ppn2_scores'],
                             self._predictions['ppn2_closest_gt_distance'],
                             self._predictions['ppn2_true_labels'],
+                            self._predictions['ppn2_true_labels_both'],
                             self._predictions['ppn2_cls_score'],
                             self._losses['loss_ppn2_point'],
                             self.summary_op
@@ -93,6 +94,8 @@ class PPN(object):
         print("ppn2_scores: ", ppn2_scores.shape, ppn2_scores[0:10])
         print("ppn2_closest_gt_distance: ", ppn2_closest_gt_distance.shape, ppn2_closest_gt_distance[0:10])
         print("ppn2_true_labels: ", ppn2_true_labels.shape, ppn2_true_labels[0:10])
+        print("ppn2_true_labels_both: ", ppn2_true_labels_both.shape, ppn2_true_labels_both[0:10])
+        print("difference=", np.sum(np.abs(ppn2_true_labels - ppn2_true_labels_both)))
         print("ppn2_cls_scores: ", ppn2_cls_score.shape, ppn2_cls_score[0:10])
         print("#positives: ", np.sum(ppn2_positives))
         loss_ppn2_point_np = np.mean(np.mean(ppn2_closest_gt_distance[ppn2_positives]))
@@ -159,8 +162,10 @@ class PPN(object):
                 tf.summary.scalar('loss_ppn2_class', self._losses['loss_ppn2_class'])
 
                 self.summary_op = tf.summary.merge_all()
-                optimizer = tf.train.AdamOptimizer(self.lr)
-                self.train_op = optimizer.minimize(total_loss)
+                global_step = tf.Variable(0, trainable=False)
+                lr = tf.train.exponential_decay(self.lr, global_step, 1000, 0.95)
+                optimizer = tf.train.AdamOptimizer(lr)
+                self.train_op = optimizer.minimize(total_loss, global_step=global_step)
 
             # Testing time
             # Turn predicted positions (float) into original image positions
@@ -372,14 +377,16 @@ class PPN(object):
                 # ground-truth point where only positives count
                 loss_ppn2_point = tf.reduce_mean(tf.reduce_mean(tf.boolean_mask(closest_gt_distance, positives)))
                 # second is a softmax class loss from both positives and negatives
-                # for positives, the true label is defined by the closest point’s label
-                loss_ppn2_class = tf.reduce_mean(tf.reduce_sum(tf.nn.sparse_softmax_cross_entropy_with_logits(labels=tf.cast(tf.reshape(true_labels, (-1,)), tf.int32),
+                # the true label is defined by the closest ground truth point’s label
+                _, _, true_labels_both = assign_gt_pixels(self.gt_pixels_placeholder, proposals2)
+                loss_ppn2_class = tf.reduce_mean(tf.reduce_sum(tf.nn.sparse_softmax_cross_entropy_with_logits(labels=tf.cast(tf.reshape(true_labels_both, (-1,)), tf.int32),
                                                                                  logits=tf.reshape(ppn2_cls_score, (-1, self.num_classes)))))
 
                 self._predictions['ppn2_positives'] = positives
                 self._losses['loss_ppn2_point'] = loss_ppn2_point
                 self._losses['loss_ppn2_class'] = loss_ppn2_class
                 self._predictions['ppn2_true_labels'] = true_labels
+                self._predictions['ppn2_true_labels_both'] = true_labels_both
                 self._predictions['ppn2_closest_gt_distance'] = closest_gt_distance
 
             return proposals2, scores2

ppn_utils.py
@@ -144,8 +144,9 @@ def compute_positives_ppn1(gt_pixels):
         gt_pixels = tf.cast(tf.floor(gt_pixels / 32.0), tf.int32)
         # Assign positive pixels based on gt_pixels
         #classes = classes + tf.scatter_nd(gt_pixels, tf.constant(value=1.0, shape=tf.shape(gt_pixels)[0]), classes.shape)
-        classes = tf.scatter_nd(gt_pixels, tf.ones(shape=(tf.shape(gt_pixels)[0], )), classes.shape)
-        classes_mask = tf.cast(tf.reshape(classes, shape=(-1, 1)), tf.bool)
+        classes = classes + tf.scatter_nd(gt_pixels, tf.fill((tf.shape(gt_pixels)[0],), 1.0), classes.shape)
+        classes = tf.cast(tf.reshape(classes, shape=(-1, 1)), tf.int32)
+        classes_mask = tf.cast(classes, tf.bool) # Turn classes into a mask
         return classes_mask
 
 def compute_positives_ppn2(scores, closest_gt_distance, true_labels, threshold=2):
@@ -161,9 +162,9 @@ def compute_positives_ppn2(scores, closest_gt_distance, true_labels, threshold=2
         pixel_count = tf.shape(true_labels)[0]
         common_shape = tf.stack([pixel_count, 1])
         predicted_labels = tf.reshape(tf.argmax(scores, axis=1, output_type=tf.int32), common_shape)
-        #assert predicted_labels.get_shape().as_list()[-1] == 1 and len(predicted_labels.get_shape().as_list()) == 2 # Shape [None, 1]
+        assert predicted_labels.get_shape().as_list()[-1] == 1 and len(predicted_labels.get_shape().as_list()) == 2 # Shape [None, 1]
         true_labels = tf.cast(true_labels, tf.int32)
-        mask = tf.where(tf.greater(closest_gt_distance, threshold), tf.zeros(common_shape, tf.bool), tf.ones(common_shape, tf.bool))
+        mask = tf.where(tf.greater(closest_gt_distance, threshold), tf.fill(common_shape, False), tf.fill(common_shape, True))
         #mask = tf.where(tf.equal(true_labels, predicted_labels), mask, tf.fill(common_shape, False))
         return mask
 
@@ -175,6 +176,10 @@ def assign_gt_pixels(gt_pixels_placeholder, proposals, rois=None, scores=None):
     Rois shape: [A, 2] coordinates in F5 feature map (16x16)
     Option roi allows to convert gt_pixels_placeholder information to ROI 4x4 coordinates
     Returns closest ground truth pixels for all pixels and corresponding distance
+    (parenthesis correspond to ppn2 ~ rois is not None)
+    -  closest_gt = index of closest gt pixel (of same class)
+    - closest_gt_distance = index of closest gt pixel (of same class)
+    - closest_gt_label = label of closest gt pixel (regardless of class)
     """
     if rois is not None and scores is None:
         raise Exception("Scores cannot be empty if ROIs is not empty")
@@ -214,11 +219,11 @@ def assign_gt_pixels(gt_pixels_placeholder, proposals, rois=None, scores=None):
         proposals = tf.expand_dims(proposals, axis=1)
         distances = tf.sqrt(tf.reduce_sum(tf.pow(proposals - all_gt_pixels, 2), axis=2))
         # distances.shape = [A*N*N, None]
-        # closest_gt.shape = [A*N*N,]
-        # closest_gt[i] = indice of closest gt in gt_pixels_placeholder
         if rois is not None:
             distances = distances + tf.scatter_nd(tf.cast(tf.where(all_gt_pixels_mask), tf.int32), tf.fill((tf.shape(tf.where(all_gt_pixels_mask))[0],), 10000.0), tf.shape(all_gt_pixels_mask))
 
+        # closest_gt.shape = [A*N*N,]
+        # closest_gt[i] = indice of closest gt in gt_pixels_placeholder
         closest_gt = tf.argmin(distances, axis=1)
         closest_gt_distance = tf.reduce_min(distances, axis=1, keep_dims=True)
         #print("squeezed gt_pixels_placeholder shape=", tf.squeeze(tf.slice(gt_pixels_placeholder, [0,0,0], [-1,1,-1]), axis=1).shape)