Fix NMS implementation and add average over all proposals inside box

faster_particles/display_utils.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 from mpl_toolkits.mplot3d import Axes3D
-from sklearn.cluster import DBSCAN
+from faster_particles.ppn_utils import filter_points
 
 def draw_voxel(x, y, z, size, ax, alpha=0.3, facecolors='pink', **kwargs):
     vertices = [
@@ -30,25 +30,6 @@ def draw_voxel(x, y, z, size, ax, alpha=0.3, facecolors='pink', **kwargs):
     poly.set_alpha(alpha)
     poly.set_facecolor(facecolors)
     ax.add_collection3d(poly)
-
-def filter_points(im_proposals, im_scores, eps):
-    db = DBSCAN(eps=eps, min_samples=1).fit_predict(im_proposals)
-    keep = {}
-    index = {}
-    new_proposals = []
-    for i in np.unique(db):
-        indices = np.where(db == i)
-        new_proposals.append(np.average(im_proposals[indices], axis=0)) # weights=im_scores[indices]
-
-    """for i in range(len(db)):
-        cluster = db[i]
-        if cluster not in keep.keys() or im_scores[i] > keep[cluster]:
-            keep[cluster] = im_scores[i]
-            index[cluster] = i
-    new_proposals = []
-    for cluster in keep:
-        new_proposals.append(im_proposals[index[cluster]])"""
-    return np.array(new_proposals)
 
 def display_original_image(blob, cfg, ax, vmin=0, vmax=400, cmap='jet'):
     # Display original image

faster_particles/ppn.py
@@ -13,7 +13,7 @@ import sys, os
 
 from faster_particles.ppn_utils import include_gt_pixels, compute_positives_ppn2, \
     compute_positives_ppn1, assign_gt_pixels, generate_anchors, \
-    predicted_pixels, top_R_pixels, slice_rois, crop_pool_layer, nms
+    predicted_pixels, top_R_pixels, slice_rois, crop_pool_layer, nms, filter_points
 from faster_particles.base_net.vgg import VGG
 
 class PPN(object):
@@ -168,27 +168,34 @@ class PPN(object):
                 with tf.variable_scope("final_proposals"):
                     im_proposals = tf.identity((proposals2 + self.dim2*rois)*self.dim1, name="im_proposals_raw")
                     im_labels = tf.argmax(scores2, axis=1, name="im_labels_raw")
-                    im_scores = tf.gather_nd(scores2, tf.concat([tf.reshape(tf.range(0, tf.shape(im_labels)[0]), (-1, 1)), tf.cast(tf.reshape(im_labels, (-1, 1)), tf.int32)], axis=1), name="im_scores_raw")
+                    #im_scores = tf.gather_nd(scores2, tf.concat([tf.reshape(tf.range(0, tf.shape(im_labels)[0]), (-1, 1)), tf.cast(tf.reshape(im_labels, (-1, 1)), tf.int32)], axis=1), name="im_scores_raw")
+                    im_scores = scores2[:, 0] + scores2[:, 1]
                     # We have now num_roi proposals and corresponding labels in original image.
                     keep = tf.where(tf.greater(im_scores, self.cfg.MIN_SCORE), name="keep_good_scores")
                     im_proposals = tf.gather_nd(im_proposals, keep, name="im_proposals")
                     im_labels = tf.gather_nd(im_labels, keep, name="im_labels")
                     im_scores = tf.gather_nd(im_scores, keep, name="im_scores")
                     # Pixel NMS equivalent ?
-                    im_proposals, im_scores = nms(im_proposals, im_scores)
+                    im_proposals, keep = nms(im_proposals, im_scores)
+                    im_proposals = tf.gather(im_proposals, keep)
+                    im_labels = tf.gather(im_labels, keep)
+                    im_scores = tf.gather(im_scores, keep)
+                    # Use DBSCAN
+                    #im_proposals, im_scores, keep = tf.py_func(filter_points, [im_proposals, im_scores, 15.0 if self.cfg.DATA_3D else 20.0], [tf.float32, tf.float32, tf.int64])
+                    #im_labels = tf.gather(im_labels, keep)
                     self._predictions['im_proposals'] = im_proposals
                     self._predictions['im_labels'] = im_labels
                     self._predictions['im_scores'] = im_scores
 
                 if self.is_training:
                     distances = tf.zeros((tf.shape(im_proposals)[0], tf.shape(self.gt_pixels_placeholder)[0]))
-                    for i in range(im_proposals.get_shape()[-1]):
+                    for i in range(self.dim):
                         x1, x2 = tf.meshgrid(self.gt_pixels_placeholder[:, i], im_proposals[:, i])
                         distances = distances + tf.pow(x1 - x2, 2)
                     distances = tf.sqrt(distances, name="final_distances")
                     closest_distance = tf.reduce_min(distances, axis=1, name="final_closest_distance")
                     # Final loss for points: mean for all proposed points of distance to closest gt point
-                    self._losses['loss_final_point'] = tf.reduce_mean(tf.reduce_mean(closest_distance), name="loss_final_point")
+                    self._losses['distance_final_point'] = tf.reduce_mean(tf.reduce_mean(closest_distance), name="distance_final_point")
 
                 # FIXME How to combine losses
                 total_loss = tf.identity(self.lambda_ppn * (self.lambda_ppn1 * self._losses['loss_ppn1_point'] \
@@ -204,15 +211,18 @@ class PPN(object):
                     tf.summary.scalar('ppn2_positives', tf.reduce_sum(tf.cast(self._predictions['ppn2_positives'], tf.float32), name="ppn2_positives"))
 
                     tf.summary.scalar('loss_ppn1_point', self._losses['loss_ppn1_point'])
-                    tf.summary.scalar('loss_ppn1_class', self._losses['loss_ppn1_class'])
+                    #tf.summary.scalar('loss_ppn1_class', self._losses['loss_ppn1_class'])
                     tf.summary.scalar('loss_ppn2_point', self._losses['loss_ppn2_point'])
-                    tf.summary.scalar('loss_ppn2_class', self._losses['loss_ppn2_class'])
-                    tf.summary.scalar('loss_ppn2_background', self._losses['loss_ppn2_background'])
-                    tf.summary.scalar('loss_ppn2_track', self._losses['loss_ppn2_track'])
-                    tf.summary.scalar('loss_ppn2_shower', self._losses['loss_ppn2_shower'])
-                    tf.summary.scalar('accuracy_class_ppn1', self._predictions['accuracy_ppn1'])
-                    tf.summary.scalar('accuracy_class_ppn2', self._predictions['accuracy_ppn2'])
-                    tf.summary.scalar('loss_final_point', self._losses['loss_final_point'])
+                    #tf.summary.scalar('loss_ppn2_class', self._losses['loss_ppn2_class'])
+                    #tf.summary.scalar('loss_ppn2_background', self._losses['loss_ppn2_background'])
+                    #tf.summary.scalar('loss_ppn2_track', self._losses['loss_ppn2_track'])
+                    #tf.summary.scalar('loss_ppn2_shower', self._losses['loss_ppn2_shower'])
+                    #tf.summary.scalar('accuracy_class_ppn1', self._predictions['accuracy_ppn1'])
+                    #tf.summary.scalar('accuracy_class_ppn2', self._predictions['accuracy_ppn2'])
+                    tf.summary.scalar('distance_final_point', self._losses['distance_final_point'])
+
+                    relative_nb_points = tf.shape(im_proposals)[0] / tf.shape(self.gt_pixels_placeholder)[0]
+                    tf.summary.scalar('relative_nb_points', relative_nb_points)
 
                     with tf.variable_scope("optimizer"):
                         global_step = tf.Variable(0, trainable=False, name="global_step")

faster_particles/ppn_utils.py
@@ -3,39 +3,52 @@
 
 import numpy as np
 import tensorflow as tf
+from sklearn.cluster import DBSCAN
 
-def nms_step(order, areas, keep, threshold, size, *args):
+def filter_points(im_proposals, im_scores, eps):
+    db = DBSCAN(eps=eps, min_samples=1).fit_predict(im_proposals)
+    keep = {}
+    index = {}
+    new_proposals = []
+    new_scores = []
+    clusters, index = np.unique(db, return_index=True)
+    for i in clusters:
+        indices = np.where(db == i)
+        new_proposals.append(np.average(im_proposals[indices], axis=0)) # weights=im_scores[indices]
+        new_scores.append(np.average(im_scores[indices], axis=0))
+    """for i in range(len(db)):
+        cluster = db[i]
+        if cluster not in keep.keys() or im_scores[i] > keep[cluster]:
+            keep[cluster] = im_scores[i]
+            index[cluster] = i
+    new_proposals = []
+    for cluster in keep:
+        new_proposals.append(im_proposals[index[cluster]])"""
+    return np.array(new_proposals), np.array(new_scores), index
+
+def nms_step(order, areas, proposals, new_proposals, keep, threshold, size, *args):
     i = order[0]
     keep = tf.concat([keep, [i]], axis=0)
     dim = len(args)/2
     inter = tf.ones((tf.shape(order)[0]-1,)) # area/volume of intersection
+    proposals_inside = proposals
     for d in range(dim):
         xx1 = tf.maximum(args[d][i], tf.gather(args[d], order[1:]))
         xx2 = tf.minimum(args[dim+d][i], tf.gather(args[dim+d], order[1:]))
         inter = inter * tf.maximum(0.0, xx2 - xx1 + 1)
+        indices_inside = tf.where(tf.logical_and(proposals_inside[:, d] >= args[d][i], proposals_inside[:, d] <= args[dim+d][i]))
+        proposals_inside = tf.gather_nd(proposals_inside, indices_inside)
+
     # Compute IoU
     ovr = inter / (tf.gather(areas, i) + tf.gather(areas, order[1:]) - inter)
     indices = tf.where(ovr <= threshold)
     new_order = tf.gather(order, indices + 1)[:, 0]
-    return (new_order, areas, keep, threshold, size) + args
+    current_coord = tf.reduce_mean(proposals_inside, axis=0)
+    #current_coord = proposals[i]
+    new_proposals = tf.concat([new_proposals[:i], [current_coord], new_proposals[i+1:]], axis=0)
+    return (new_order, areas, proposals, new_proposals, keep, threshold, size) + args
 
-def nms_step3d(order, x1, y1, z1, x2, y2, areas, keep, threshold, size):
-    i = order[0]
-    keep = tf.concat([keep, [i]], axis=0)
-    xx1 = tf.maximum(x1[i], tf.gather(x1, order[1:]))
-    yy1 = tf.maximum(y1[i], tf.gather(y1, order[1:]))
-    xx2 = tf.minimum(x2[i], tf.gather(x2, order[1:]))
-    yy2 = tf.minimum(y2[i], tf.gather(y2, order[1:]))
-
-    w = tf.maximum(0.0, xx2 - xx1 + 1)
-    h = tf.maximum(0.0, yy2 - yy1 + 1)
-    inter = w * h
-    ovr = inter / (tf.gather(areas, i) + tf.gather(areas, order[1:]) - inter)
-    indices = tf.where(ovr <= threshold)
-    new_order = tf.gather(order, indices + 1)[:, 0]
-    return new_order, x1, y1, x2, y2, areas, keep, threshold, size
-
-def nms(im_proposals, im_scores, threshold=0.000000001, size=5.0):
+def nms(im_proposals, im_scores, threshold=0.01, size=4.0):
     """
     Performs NMS (non maximal suppression) on proposed pixels.
     - Look at pixels in order of decreasing score
@@ -47,9 +60,9 @@ def nms(im_proposals, im_scores, threshold=0.000000001, size=5.0):
     coords = ()
     dim = im_proposals.get_shape()[-1]
     for d in range(dim):
-        coords = coords + (im_proposals[:, 0] - size,)
+        coords = coords + (im_proposals[:, d] - size,)
     for d in range(dim):
-        coords = coords + (im_proposals[:, 0] + size,)
+        coords = coords + (im_proposals[:, d] + size,)
     for d in range(dim):
         areas = areas * (coords[dim+d] - coords[d] + 1.0)
     coords_shape = ()
@@ -59,9 +72,11 @@ def nms(im_proposals, im_scores, threshold=0.000000001, size=5.0):
     keep = tf.Variable([0], dtype=tf.int32)
     threshold = tf.constant(threshold)
     size = tf.constant(size)
-    keep = tf.while_loop(lambda order, *args: tf.shape(order)[0] > 0, nms_step, [order, areas, keep, threshold, size] + list(coords), shape_invariants=[order.get_shape(), areas.get_shape(), tf.TensorShape((None,)), threshold.get_shape(), size.get_shape()] + list(coords_shape))[2]
-    keep = keep[1:]
-    return tf.gather(im_proposals, keep), tf.gather(im_scores, keep)
+    #new_proposals = tf.Variable(im_proposals, validate_shape=False)
+    while_return = tf.while_loop(lambda order, *args: tf.shape(order)[0] > 0, nms_step, [order, areas, im_proposals, im_proposals, keep, threshold, size] + list(coords), shape_invariants=[order.get_shape(), areas.get_shape(), im_proposals.get_shape(), im_proposals.get_shape(), tf.TensorShape((None,)), threshold.get_shape(), size.get_shape()] + list(coords_shape))
+    keep = while_return[4][1:]
+    new_proposals = while_return[3]
+    return new_proposals, keep
 
 def generate_anchors(im_shape, repeat=1):
     """
@@ -254,10 +269,10 @@ def assign_gt_pixels(gt_pixels_placeholder, proposals, dim1, dim2, rois=None):
         # convert proposals to real image coordinates in order to compare with
         # ground truth pixels coordinates
         if rois is not None: # means PPN2
-            # Convert to F3 coordinates
+            # Convert from F3 coordinates
             proposals = (proposals + dim2 * rois) * dim1
         else:
-            # Convert to F5 coordinates
+            # Convert from F5 coordinates
             proposals = proposals * dim1 * dim2
 
         # Tile to have shape (A*N*N, None, 2)